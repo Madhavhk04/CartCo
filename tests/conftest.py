4b281de2c2d59171cce83278a4ab84b6fdc0a441
Madhavhk04 <devuappu@gmail.com>
2026-06-30 01:13:44 +0530

    ci: configure spark.jars.packages in test session to resolve delta class path errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ def spark():
     spark = SparkSession.builder \
         .master("local[*]") \
         .appName("CartCo-Test-Suite") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .config("spark.sql.shuffle.partitions", "1") \
